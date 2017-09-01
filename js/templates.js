1f0097ca56698d988e3a35dbc7571a41e40a9d8e
Kia Farhang <kfarhang0@gmail.com>
2017-09-01 13:49:41 -0500

    Made chatter optional in iFrame template

--- js/templates.js
+++ js/templates.js
@@ -31,17 +31,22 @@ templates['iframe'] = template({"1":function(container,depth0,helpers,partials,d
   return "        <h2 class=\"freeform-iframe-headline\">"
     + container.escapeExpression(((helper = (helper = helpers.headline || (depth0 != null ? depth0.headline : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"headline","hash":{},"data":data}) : helper)))
     + "</h2>\n";
+},"7":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "            <p class=\"freeform-iframe-subhead\">"
+    + container.escapeExpression(((helper = (helper = helpers.chatter || (depth0 != null ? depth0.chatter : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"chatter","hash":{},"data":data}) : helper)))
+    + "</p>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function";
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
   return "<xmp>\n            <style>\n            h3.freeform-iframe-exclusive {\n                text-align: center;\n            }\n\n            h2.freeform-iframe-headline {\n                font-family: Pierpont-bold, serif;\n                font-size: 44px;\n                line-height: 38px;\n                margin: 1% auto;\n                text-align: center;\n            }\n\n            p.freeform-iframe-subhead {\n                font-family: Georgia, serif;\n                font-size: 18px;\n                text-align: center;\n            }\n\n            .vidWrapper {\n                margin-bottom: 1em;\n                position: relative;\n                padding-bottom: 56.25%;\n                padding-top: 25px;\n                height: 0;\n            }\n\n            .vidWrapper iframe {\n                position: absolute;\n                top: 0;\n                left: 0;\n                width: 100%;\n                height: 94%;\n            }\n            </style>\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.exclusive : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.storyLink : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
-    + "            <p class=\"freeform-iframe-subhead\">"
-    + container.escapeExpression(((helper = (helper = helpers.chatter || (depth0 != null ? depth0.chatter : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"chatter","hash":{},"data":data}) : helper)))
-    + "</p>\n            <div class=\"vidWrapper\">\n                "
-    + ((stack1 = ((helper = (helper = helpers.iframe || (depth0 != null ? depth0.iframe : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"iframe","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.chatter : depth0),{"name":"if","hash":{},"fn":container.program(7, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "            <div class=\"vidWrapper\">\n                "
+    + ((stack1 = ((helper = (helper = helpers.iframe || (depth0 != null ? depth0.iframe : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"iframe","hash":{},"data":data}) : helper))) != null ? stack1 : "")
     + "\n            </div>\n        </xmp>";
 },"useData":true});
 templates['story'] = template({"1":function(container,depth0,helpers,partials,data) {
