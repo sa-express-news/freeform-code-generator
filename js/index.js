5a1c7d5d277f975d0d573d9769490a6bdc5629dd
Kia Farhang <kfarhang0@gmail.com>
2017-09-29 14:40:00 -0500

    Made dropcap in story template optional

--- js/index.js
+++ js/index.js
@@ -1,166 +1,168 @@
-    document.getElementById('iframe-form').addEventListener('submit', iframeSubmit);
-    document.getElementById('copy-iframe-code').addEventListener('click', copyiFrameCode);
+document.getElementById('iframe-form').addEventListener('submit', iframeSubmit);
+document.getElementById('copy-iframe-code').addEventListener('click', copyiFrameCode);
 
-    document.getElementById('story-form').addEventListener('submit', storySubmit);
-    document.getElementById('copy-story-code').addEventListener('click', copyStoryCode);
+document.getElementById('story-form').addEventListener('submit', storySubmit);
+document.getElementById('copy-story-code').addEventListener('click', copyStoryCode);
 
-    document.getElementById('atf-form').addEventListener('submit', atfSubmit);
-    document.getElementById('copy-atf-code').addEventListener('click', copyAtfCode);
+document.getElementById('atf-form').addEventListener('submit', atfSubmit);
+document.getElementById('copy-atf-code').addEventListener('click', copyAtfCode);
 
-    function iframeSubmit(event) {
-        event.preventDefault();
-        var headline = document.getElementById('iframeheadline').value;
-        var chatter = document.getElementById('iframechatter').value;
-        var link = document.getElementById('iframelink').value.trim();
-        var iFrameCode = document.getElementById('iframeiframe').value.trim();
-        var iframe = iFrameCode.indexOf('<div') > -1 ? scrubDivFromIframe(iFrameCode) : iFrameCode;
-        var exclusive = document.getElementById('iframe-exclusive').checked;
+function iframeSubmit(event) {
+    event.preventDefault();
+    var headline = document.getElementById('iframeheadline').value;
+    var chatter = document.getElementById('iframechatter').value;
+    var link = document.getElementById('iframelink').value.trim();
+    var iFrameCode = document.getElementById('iframeiframe').value.trim();
+    var iframe = iFrameCode.indexOf('<div') > -1 ? scrubDivFromIframe(iFrameCode) : iFrameCode;
+    var exclusive = document.getElementById('iframe-exclusive').checked;
 
-        var context = {
-            storyLink: link,
-            headline: headline,
-            chatter: chatter,
-            iframe: iframe,
-            exclusive: exclusive
-        };
+    var context = {
+        storyLink: link,
+        headline: headline,
+        chatter: chatter,
+        iframe: iframe,
+        exclusive: exclusive
+    };
 
-        generateiFrameCode(context);
-        showCopyButton();
+    generateiFrameCode(context);
+    showCopyButton();
 
-        function showCopyButton() {
-            var button = document.getElementById('copy-iframe-code');
-            button.style.display = 'block';
-        }
-
-        function generateiFrameCode(context) {
-            var html = Handlebars.templates.iframe(context);
-            var div = document.getElementsByClassName('iframe-code-container')[0];
-            div.innerHTML = html;
-        }
-
-
+    function showCopyButton() {
+        var button = document.getElementById('copy-iframe-code');
+        button.style.display = 'block';
     }
 
-    function storySubmit(event) {
-        event.preventDefault();
-        var headline = document.getElementById('storyHeadline').value;
-        var firstLetter = document.getElementById('storyChatter').value.substring(0, 1);
-        var chatter = document.getElementById('storyChatter').value.substring(1);
-        var link = document.getElementById('storyLink').value.trim();
-        var image = document.getElementById('storyImage').value.trim();
-        var exclusive = document.getElementById('storyExclusive').checked;
-
-        var context = {
-            storyLink: link,
-            headline: headline,
-            firstLetter: firstLetter,
-            chatter: chatter,
-            image: image,
-            exclusive: exclusive
-        };
-
-        generateStoryCode(context);
-        showCopyButton();
-
-        function showCopyButton() {
-            var button = document.getElementById('copy-story-code');
-            button.style.display = 'block';
-        }
-
-        function generateStoryCode(context) {
-            var html = Handlebars.templates.story(context);
-            var div = document.getElementsByClassName('story-code-container')[0];
-            div.innerHTML = html;
-        }
-
-    }
-
-    function atfSubmit(event) {
-        event.preventDefault();
-        var headline = document.getElementById('atfHeadline').value;
-        var link = document.getElementById('atfLink').value.trim();
-        var image = document.getElementById('atfImage').value.trim();
-        var exclusive = document.getElementById('atfExclusive').checked;
-
-        var context = {
-            storyLink: link,
-            headline: headline,
-            image: image,
-            exclusive: exclusive
-        };
-
-        generateAtfCode(context);
-        showCopyButton();
-
-        function showCopyButton() {
-            var button = document.getElementById('copy-atf-code');
-            button.style.display = 'block';
-        }
-
-        function generateAtfCode(context) {
-            var html = Handlebars.templates.atf(context);
-            var div = document.getElementsByClassName('atf-code-container')[0];
-            div.innerHTML = html;
-        }
-
-    }
-
-    function copyiFrameCode() {
-        var textArea = document.createElement('textarea');
-        var stringWithoutXmp = clearXmpFromString(document.getElementsByClassName('iframe-code-container')[0].innerHTML);
-        textArea.value = stringWithoutXmp;
-        document.body.appendChild(textArea);
-        textArea.select();
-        try {
-            document.execCommand('copy');
-            document.body.removeChild(textArea);
-        } catch (err) {
-            console.log(err);
-        }
-
-    }
-
-    function copyStoryCode() {
-        var textArea = document.createElement('textarea');
-        var stringWithoutXmp = clearXmpFromString(document.getElementsByClassName('story-code-container')[0].innerHTML);
-        textArea.value = stringWithoutXmp;
-        document.body.appendChild(textArea);
-        textArea.select();
-        try {
-            document.execCommand('copy');
-            document.body.removeChild(textArea);
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    function copyAtfCode() {
-        var textArea = document.createElement('textarea');
-        var stringWithoutXmp = clearXmpFromString(document.getElementsByClassName('atf-code-container')[0].innerHTML);
-        textArea.value = stringWithoutXmp;
-        document.body.appendChild(textArea);
-        textArea.select();
-        try {
-            document.execCommand('copy');
-            document.body.removeChild(textArea);
-        } catch (err) {
-            console.log(err);
-        }
+    function generateiFrameCode(context) {
+        var html = Handlebars.templates.iframe(context);
+        var div = document.getElementsByClassName('iframe-code-container')[0];
+        div.innerHTML = html;
     }
 
 
-    function clearXmpFromString(string) {
-        var open = '<xmp>';
-        var close = '</xmp>';
-        var openRE = new RegExp(open, 'g');
-        var closeRE = new RegExp(close, 'g');
+}
 
-        return string.replace(openRE, '').replace(closeRE, '');
+function storySubmit(event) {
+    event.preventDefault();
+    var headline = document.getElementById('storyHeadline').value;
+    var firstLetter = document.getElementById('storyChatter').value.substring(0, 1);
+    var chatter = document.getElementById('storyChatter').value.substring(1);
+    var link = document.getElementById('storyLink').value.trim();
+    var image = document.getElementById('storyImage').value.trim();
+    var exclusive = document.getElementById('storyExclusive').checked;
+    var dropcap = document.getElementById('storyDropcap').checked;
+
+    var context = {
+        storyLink: link,
+        headline: headline,
+        firstLetter: firstLetter,
+        chatter: chatter,
+        image: image,
+        exclusive: exclusive,
+        dropcap: dropcap
+    };
+
+    generateStoryCode(context);
+    showCopyButton();
+
+    function showCopyButton() {
+        var button = document.getElementById('copy-story-code');
+        button.style.display = 'block';
     }
 
-    function scrubDivFromIframe(string) {
-        var div = document.createElement('div');
-        div.innerHTML = string;
-        div.innerHTML = div.getElementsByTagName('div')[0].innerHTML;
-        return div.innerHTML;
+    function generateStoryCode(context) {
+        var html = Handlebars.templates.story(context);
+        var div = document.getElementsByClassName('story-code-container')[0];
+        div.innerHTML = html;
     }
+
+}
+
+function atfSubmit(event) {
+    event.preventDefault();
+    var headline = document.getElementById('atfHeadline').value;
+    var link = document.getElementById('atfLink').value.trim();
+    var image = document.getElementById('atfImage').value.trim();
+    var exclusive = document.getElementById('atfExclusive').checked;
+
+    var context = {
+        storyLink: link,
+        headline: headline,
+        image: image,
+        exclusive: exclusive
+    };
+
+    generateAtfCode(context);
+    showCopyButton();
+
+    function showCopyButton() {
+        var button = document.getElementById('copy-atf-code');
+        button.style.display = 'block';
+    }
+
+    function generateAtfCode(context) {
+        var html = Handlebars.templates.atf(context);
+        var div = document.getElementsByClassName('atf-code-container')[0];
+        div.innerHTML = html;
+    }
+
+}
+
+function copyiFrameCode() {
+    var textArea = document.createElement('textarea');
+    var stringWithoutXmp = clearXmpFromString(document.getElementsByClassName('iframe-code-container')[0].innerHTML);
+    textArea.value = stringWithoutXmp;
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+function copyStoryCode() {
+    var textArea = document.createElement('textarea');
+    var stringWithoutXmp = clearXmpFromString(document.getElementsByClassName('story-code-container')[0].innerHTML);
+    textArea.value = stringWithoutXmp;
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function copyAtfCode() {
+    var textArea = document.createElement('textarea');
+    var stringWithoutXmp = clearXmpFromString(document.getElementsByClassName('atf-code-container')[0].innerHTML);
+    textArea.value = stringWithoutXmp;
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+function clearXmpFromString(string) {
+    var open = '<xmp>';
+    var close = '</xmp>';
+    var openRE = new RegExp(open, 'g');
+    var closeRE = new RegExp(close, 'g');
+
+    return string.replace(openRE, '').replace(closeRE, '');
+}
+
+function scrubDivFromIframe(string) {
+    var div = document.createElement('div');
+    div.innerHTML = string;
+    div.innerHTML = div.getElementsByTagName('div')[0].innerHTML;
+    return div.innerHTML;
+}
--- js/templates.js
+++ js/templates.js
@@ -19,7 +19,7 @@ templates['atf'] = template({"1":function(container,depth0,helpers,partials,data
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
-  return "<xmp>\n  <style>\n    img.freeform-atf-photo {\n      margin: 1em 0;\n      width: 100%;\n    }\n\n    h3.freeform-exclusive {\n      text-align: center;\n    }\n\n    h2.freeform-atf-headline {\n      font-family: Pierpont-bold, serif;\n      font-size: 29px;\n      font-weight: normal;\n      line-height: 38px;\n      margin: 1% auto;\n      text-align: center;\n    }\n\n    @media (min-width: 1280px) {\n      h2.freeform-atf-headline {\n        font-size: 26px;\n      }\n    }\n  </style>\n\n"
+  return "<xmp>\n  <style>\n    img.freeform-atf-photo {\n      margin: 1em 0;\n      width: 100%;\n    }\n\n    h3.freeform-exclusive {\n      text-align: center;\n    }\n\n    h2.freeform-atf-headline {\n      font-family: Pierpont-bold, serif;\n      font-size: 29px;\n      font-weight: normal;\n      line-height: 31px;\n      margin: 1% auto;\n      text-align: center;\n    }\n\n    @media (min-width: 1280px) {\n      h2.freeform-atf-headline {\n        font-size: 26px;\n      }\n    }\n  </style>\n\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.exclusive : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + " "
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.storyLink : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
@@ -28,11 +28,11 @@ templates['atf'] = template({"1":function(container,depth0,helpers,partials,data
     + "\">\n</xmp>";
 },"useData":true});
 templates['iframe'] = template({"1":function(container,depth0,helpers,partials,data) {
-    return "            <h3 class=\"freeform-iframe-exclusive section-name\">ExpressNews.com Exclusive</h3> ";
+    return "    <h3 class=\"freeform-iframe-exclusive section-name\">ExpressNews.com Exclusive</h3> ";
 },"3":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "            <h2 class=\"freeform-iframe-headline\"><a href=\""
+  return "\n    <h2 class=\"freeform-iframe-headline\"><a href=\""
     + alias4(((helper = (helper = helpers.storyLink || (depth0 != null ? depth0.storyLink : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"storyLink","hash":{},"data":data}) : helper)))
     + "\">"
     + alias4(((helper = (helper = helpers.headline || (depth0 != null ? depth0.headline : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"headline","hash":{},"data":data}) : helper)))
@@ -40,44 +40,57 @@ templates['iframe'] = template({"1":function(container,depth0,helpers,partials,d
 },"5":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return "        <h2 class=\"freeform-iframe-headline\">"
+  return "    <h2 class=\"freeform-iframe-headline\">"
     + container.escapeExpression(((helper = (helper = helpers.headline || (depth0 != null ? depth0.headline : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"headline","hash":{},"data":data}) : helper)))
-    + "</h2>\n";
+    + "</h2>\n    ";
 },"7":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return "            <p class=\"freeform-iframe-subhead\">"
+  return "\n    <p class=\"freeform-iframe-subhead\">"
     + container.escapeExpression(((helper = (helper = helpers.chatter || (depth0 != null ? depth0.chatter : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"chatter","hash":{},"data":data}) : helper)))
     + "</p>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
-  return "<xmp>\n            <style>\n            h3.freeform-iframe-exclusive {\n                text-align: center;\n            }\n\n            h2.freeform-iframe-headline {\n                font-family: Pierpont-bold, serif;\n                font-size: 44px;\n                line-height: 38px;\n                margin: 1% auto;\n                text-align: center;\n            }\n\n            p.freeform-iframe-subhead {\n                font-family: Georgia, serif;\n                font-size: 18px;\n                text-align: center;\n            }\n\n            .vidWrapper {\n                margin-bottom: 1em;\n                position: relative;\n                padding-bottom: 56.25%;\n                padding-top: 25px;\n                height: 0;\n            }\n\n            .vidWrapper iframe {\n                position: absolute;\n                top: 0;\n                left: 0;\n                width: 100%;\n                height: 94%;\n            }\n            </style>\n"
+  return "<xmp>\n    <style>\n        h3.freeform-iframe-exclusive {\n            text-align: center;\n        }\n\n        h2.freeform-iframe-headline {\n            font-family: Pierpont-bold, serif;\n            font-size: 42px;\n            line-height: 36px;\n            margin: 1% auto;\n            text-align: center;\n        }\n\n        p.freeform-iframe-subhead {\n            font-family: Georgia, serif;\n            font-size: 18px;\n            text-align: center;\n        }\n\n        .vidWrapper {\n            margin-bottom: 1em;\n            position: relative;\n            padding-bottom: 56.25%;\n            padding-top: 25px;\n            height: 0;\n        }\n\n        .vidWrapper iframe {\n            position: absolute;\n            top: 0;\n            left: 0;\n            width: 100%;\n            height: 94%;\n        }\n    </style>\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.exclusive : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "\n"
+    + " "
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.storyLink : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
+    + " "
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.chatter : depth0),{"name":"if","hash":{},"fn":container.program(7, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "            <div class=\"vidWrapper\">\n                "
+    + "    <div class=\"vidWrapper\">\n        "
     + ((stack1 = ((helper = (helper = helpers.iframe || (depth0 != null ? depth0.iframe : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"iframe","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "\n            </div>\n        </xmp>";
+    + "\n    </div>\n</xmp>";
 },"useData":true});
 templates['story'] = template({"1":function(container,depth0,helpers,partials,data) {
     return "  <h3 class=\"section-name freeform-exclusive\" style=\"text-align:center;\">ExpressNews.com Exclusive</h3>\n";
-},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+},"3":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<xmp>\n<style>\n\n  img.freeform-cp-photo {\n    margin: 1em 0;\n    width: 100%;\n  }\n\n  h3.freeform-exclusive{\n    text-align: center;\n  }\n \n  h2.freeform-cp-headline {\n    font-family: Pierpont-bold, serif;\n    font-size: 44px;\n    line-height: 38px;\n    margin: 1% auto;\n    text-align: center;\n  }\n \n  @media (min-width: 1280px) {\n    h2.freeform-cp-headline {\n      font-size: 35px;\n    }\n  }\n \n  p.freeform-cp-subhead {\n    font-family: Georgia, serif;\n    font-size: 18px;\n  }\n \n  span.freeform-cp-dropcap {\n    font-family: Pierpont-bold, serif;\n    font-size: 32px;\n    font-weight: bold;\n  }\n</style>\n\n"
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.exclusive : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "\n<a href=\""
-    + alias4(((helper = (helper = helpers.storyLink || (depth0 != null ? depth0.storyLink : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"storyLink","hash":{},"data":data}) : helper)))
-    + "\">\n  <h2 class=\"freeform-cp-headline\">"
-    + alias4(((helper = (helper = helpers.headline || (depth0 != null ? depth0.headline : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"headline","hash":{},"data":data}) : helper)))
-    + "</h2></a>\n\n <img class=\"freeform-cp-photo\" src=\""
-    + alias4(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data}) : helper)))
-    + "\">\n\n <p class=\"freeform-cp-subhead\">\n  <span class=\"freeform-cp-dropcap\">"
+  return "    <span class=\"freeform-cp-dropcap\">"
     + alias4(((helper = (helper = helpers.firstLetter || (depth0 != null ? depth0.firstLetter : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"firstLetter","hash":{},"data":data}) : helper)))
     + "</span>"
     + alias4(((helper = (helper = helpers.chatter || (depth0 != null ? depth0.chatter : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"chatter","hash":{},"data":data}) : helper)))
-    + "\n </p>\n</xmp>";
+    + " ";
+},"5":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return " "
+    + alias4(((helper = (helper = helpers.firstLetter || (depth0 != null ? depth0.firstLetter : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"firstLetter","hash":{},"data":data}) : helper)))
+    + alias4(((helper = (helper = helpers.chatter || (depth0 != null ? depth0.chatter : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"chatter","hash":{},"data":data}) : helper)));
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<xmp>\n  <style>\n    img.freeform-cp-photo {\n      margin: 1em 0;\n      width: 100%;\n    }\n\n    h3.freeform-exclusive {\n      text-align: center;\n    }\n\n    h2.freeform-cp-headline {\n      font-family: Pierpont-bold, serif;\n      font-size: 42px;\n      line-height: 36px;\n      margin: 1% auto;\n      text-align: center;\n    }\n\n    @media (min-width: 1280px) {\n      h2.freeform-cp-headline {\n        font-size: 33px;\n      }\n    }\n\n    p.freeform-cp-subhead {\n      font-family: Georgia, serif;\n      font-size: 18px;\n    }\n\n    span.freeform-cp-dropcap {\n      font-family: Pierpont-bold, serif;\n      font-size: 32px;\n      font-weight: bold;\n    }\n  </style>\n\n"
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.exclusive : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "\n  <a href=\""
+    + alias4(((helper = (helper = helpers.storyLink || (depth0 != null ? depth0.storyLink : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"storyLink","hash":{},"data":data}) : helper)))
+    + "\">\n    <h2 class=\"freeform-cp-headline\">"
+    + alias4(((helper = (helper = helpers.headline || (depth0 != null ? depth0.headline : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"headline","hash":{},"data":data}) : helper)))
+    + "</h2>\n  </a>\n\n  <img class=\"freeform-cp-photo\" src=\""
+    + alias4(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data}) : helper)))
+    + "\">\n\n  <p class=\"freeform-cp-subhead\">\n"
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.dropcap : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
+    + "\n  </p>\n</xmp>";
 },"useData":true});
 })();
